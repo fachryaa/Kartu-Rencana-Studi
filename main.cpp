c053112fe84a113c37b0792bd2bf413144cf5505
fachryaa <fajri.fr85@gmail.com>
2020-04-17 19:10:12 +0700

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
-#define MAX 20
+#define MAX 24
 
 class KRS{
 private:
@@ -30,25 +33,31 @@ public:
 };
 
 void KRS::loginPage() {
-    cout<<"=========================== Login ==========================="<<endl<<endl;
     string nim,pass;
-
     menu:
+    cout<<"=========================== Login ==========================="<<endl<<endl;
+
     cout<<"Username\t: ";
     cin>>nim;
     cout<<"Password\t: ";
     cin>>pass;
-    if(!isTrue(nim,pass)) goto menu;
+    if(!isTrue(nim,pass)) {
+        goto menu;
+    }
 }
 
 bool KRS::isTrue(string nim, string pass) {
     if(nim==NIM && pass==password) {
         cout<<"\nLogin berhasil..."<<endl<<endl;
+        system("pause");
+        system("cls");
         return true;
     }
     else{
         cout<<"\nUsername atau password yang anda masukan salah..."<<endl
-            <<"Silahkan masukan kembali..."<<endl<<endl;
+                <<"Silahkan masukan kembali..."<<endl<<endl;
+        system("pause");
+        system("cls");
         return false;
     }
 }
@@ -56,19 +65,25 @@ bool KRS::isTrue(string nim, string pass) {
 //=================================================================================================
 
 void KRS::inputIp() {
-    cout<<"=========================== Input IP ==========================="<<endl<<endl;
     float ip;
+
     menu:
+    cout<<"=========================== Input IP ==========================="<<endl<<endl;
+
     cout<<"Masukan IP\t: ";
     cin>>ip;
     if(ip > 4) {
         cout<<"\nIP yang anda masukan melebihi batas..."<<endl
             <<"Silahkan masukan kembali..."<<endl<<endl;
+        system("pause");
+        system("cls");
         goto menu;
     }
     else {
         setIp(ip);
         cout<<"\nBerhasil memasukan ip..."<<endl<<endl;
+        system("pause");
+        system("cls");
     }
 }
 
@@ -99,54 +114,72 @@ void KRS::setIp(float IP) {
 //=================================================================================================
 
 void KRS::inputMatkul() {
-    cout<<"====================== Input Matkul dan SKS ======================"<<endl<<endl;
     string matkul;
     int sks;
-    bool done = true;
     int pilih;
 
-    cout<<batasSKS[0]<<" - "<<batasSKS[1]<<" SKS yang dapat anda ambil..."<<endl
-        <<"Silahkan masukan matkul dan SKS..."<<endl<<endl;
 
-    while (countSKS < batasSKS[1] && done){
+    while (countSKS < batasSKS[1]){
+        cout<<"====================== Input Matkul dan SKS ======================"<<endl<<endl;
+
+        cout<<batasSKS[0]<<" - "<<batasSKS[1]<<" SKS yang dapat anda ambil..."<<endl
+            <<"Silahkan masukan matkul dan SKS..."<<endl<<endl
+            <<"\nSisa SKS yang dapat anda ambil : "<<batasSKS[1]-countSKS<<endl<<endl;
+
+        if(countSKS >= batasSKS[0] && countSKS!=batasSKS[1]){
+            cout<<"Apakah ingin menambahkan mata kuliah lagi?\n1. Ya\n2. Tidak\nPilih [1/2] : ";
+            cin>>pilih;
+            cout<<endl;
+            if(pilih == 2) {
+                cout<<"Berhasil memasukan matkul dan SKS..."<<endl<<endl;
+                break;
+            }
+        }
+
         cin.get();
         cout<<"Matkul\t: ";
         getline(cin,matkul);
         cout<<"SKS\t: ";
         cin>>sks;
         setMatkul(matkul,sks);
-        if(countSKS >= batasSKS[0] && countSKS!=batasSKS[1]){
-            cout<<"Apakah ingin menambahkan mata kuliah lagi?\n1. Ya\n2. Tidak\nPilih [1/2] : ";
-            cin>>pilih;
-            cout<<endl;
-            if(pilih == 2) done = false;
-        }
     }
-    cout<<"Berhasil memasukan matkul dan SKS..."<<endl<<endl;
 }
 
 void KRS::setMatkul(string matkul, int sks) {
     if(countSKS+sks > batasSKS[1]){
         cout<<"\nERROR : SKS yang anda masukan melebihi peraturan IP..."<<endl
             <<"Silahkan masukan kembali..."<<endl<<endl;
+        system("pause");
+        system("cls");
     }
     else {
+        transform(matkul.begin() , matkul.end() , matkul.begin() , ::toupper);
         Matkul[countMatkul] = matkul;
         SKS[countMatkul] = sks;
         countMatkul++;
         countSKS+=sks;
-        if(countSKS != batasSKS[1])
-            cout<<"\nSisa SKS yang dapat anda ambil : "<<batasSKS[1]-countSKS<<endl<<endl;
+        if(countSKS==batasSKS[1]) {
+            cout<<"\nBerhasil memasukan matkul dan SKS..."<<endl<<endl;
+            system("pause");
+            system("cls");
+        }
+        else{
+            system("pause");
+            system("cls");
+        }
     }
 }
 
 //=================================================================================================
 
 void KRS::printMatkul() {
+    system("pause");
+    system("cls");
     cout<<"==================== Cetak Matkul dan SKS ===================="<<endl<<endl;
+    cout<<"=========================================="<<endl;
     for(int i=0 ; i<countMatkul ; i++){
-        cout<<Matkul[i]<<endl
-            <<"SKS : "<<SKS[i]<<endl<<endl
+        cout<<"Matkul\t: "<<Matkul[i]<<endl
+            <<"SKS\t: "<<SKS[i]<<endl
             <<"=========================================="<<endl;
     }
 }
@@ -157,7 +190,6 @@ void KRS::main() {
     inputMatkul();
     printMatkul();
 }
-
 
 int main() {
     KRS krs;
